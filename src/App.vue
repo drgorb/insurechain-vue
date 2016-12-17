921168ececdaad5f42b8563cdba38cfb8e1d0426
drgorb <micha.roon@managination.com>
2016-12-17 05:23:44 +0000

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <div id="app">
-    <home></home>
+  <div id="app" class="container" v-md-theme="theme">
+    <sideNav></sideNav>
+    <div class="md-toolbar-container">
+      <md-toolbar>
+        <md-button class="md-icon-button">
+          <md-icon>menu</md-icon>
+        </md-button>
+  
+        <h2 class="md-title" style="flex: 1">Default</h2>
+  
+        <md-button class="md-icon-button">
+          <md-icon>favorite</md-icon>
+        </md-button>
+      </md-toolbar>
+    </div>
   </div>
 </template>
 
 <script>
-import home from 'components/main'
+import sideNav from './components/side-nav'
+import {default as insurechain} from './ethereum/insurechain.js'
+
 export default {
   name: 'app',
   methods: {
+    register () {
+      insurechain.getInfo('123')
+    }
+  },
+  data () {
+    return {
+      toolbar: true,
+      theme: 'default'
+    }
   },
   components: {
-    home
+    sideNav
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -2,18 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
+import VueRouter from 'vue-router'
 import 'vue-material/dist/vue-material.css'
 import App from 'src/App'
 
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
 
 Vue.material.theme.register('default', {
   primary: 'blue',
   accent: 'red'
 })
 
+// 2. Define route components
+const Home = { template: '<App/>', components: { App } }
+const Retailer = { template: '<div>retailer</div>' }
+const Workshop = { template: '<div>workshop</div>' }
+
+// 3. Create the router
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/workshop', component: Workshop },
+    { path: '/retailer', component: Retailer }
+  ]
+})
+
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   template: '<App/>',
   components: { App }
